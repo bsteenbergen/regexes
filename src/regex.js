a741ae84954c355d4a5ea58ff18a1c1e8cf07c74
hallegv <hallevogelpohl@gmail.com>
2022-04-25 17:38:52 -0700

    added divisibleBy32

--- src/regex.js
+++ src/regex.js
@@ -6,7 +6,7 @@ const regexes = {
     /^5[1-5][0-9]{14}|(222[1-9]|22[3-9]\d|2[3-6]\d{2}|27[0-1]\d|2720)[0-9]{12}$/,
   adaFloat: "",
   notThreeEndingInOO: /^(?!.*(.[oO]{2})).*/,
-  divisibleBy32: "",
+  divisibleBy32: /^0{1,4}$|^[01]*00000$/,
   sevenThroughThirtyOne: /^[7-9]|[12]\d|3[01]$/,
   mLComment: /^\(\*.*\*\)$/,
   notFileForFirstNoLookAround: "",
@@ -14,7 +14,7 @@ const regexes = {
     /^\b(?:[a-eg-z]|f(?!ile|or|irst\b))\w*\b((?!file|for|first).*)$/,
   cOctal: "",
   restrictedFloatingPoint: /^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$/, // rn this is just floating point #s
-  palindrome2358: /^(.).?\1$|^(.)(.)(.){3}$|^(.)(.)(.)(.)(.){4}$/,
+  palindrome2358: /^(.)(?:(.)(?:(.)\3|\2?\2))?\1$/,
   noNegativeIntLits: /(?<![-\d])(\d+)/,
   repeated: /^^([a-z]*)\1$/,
 }
@@ -29,10 +29,11 @@ export function matches(name, string) {
  * visa
  * noNegativeIntLits
  * repeated
+ * divisibleBy32
  * 
  * IN PROG
  * masterCard    
- * notTHreeEndingIn00
+ * notThreeEndingIn00
  * sevenThroughThirtyOne
  * mLComment
  * restrictedFloatingPoint
@@ -40,7 +41,6 @@ export function matches(name, string) {
  * 
  * TODO
  * adaFloat
- * divisibleBy32
  * notFileForFirstNoLookAround
  * c0ctal
  * notFileForFirstWithLookAround
